2084fe048ddc4bf94b8ae6cc63f443c3583eeb0c
Ho-Van-Con <hovancon1998@gmail.com>
2019-04-05 16:35:55 +0700

    Fix imshow

--- read.py
+++ read.py
@@ -8,47 +8,6 @@ import imutils
 from imutils.object_detection import non_max_suppression
 
 
-#------------------------------------------------------------- SORT CONTOURS ---------------------------------------------------------
-def sort_contours(cnts, method="left-to-right"):
-	# initialize the reverse flag and sort index
-	reverse = False
-	i = 0
-
-	# handle if we need to sort in reverse
-	if method == "right-to-left" or method == "bottom-to-top":
-		reverse = True
-
-	# handle if we are sorting against the y-coordinate rather than
-	# the x-coordinate of the bounding box
-	if method == "top-to-bottom" or method == "bottom-to-top":
-		i = 1
-
-	# construct the list of bounding boxes and sort them from top to
-	# bottom
-	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-		key=lambda b:b[1][i], reverse=reverse))
-
-	# return the list of sorted contours and bounding boxes
-	return (cnts, boundingBoxes)
-
-#------------------------------------------------------------- DRAW CONTOURS ---------------------------------------------------------
-
-
-def draw_contour(image, c, i):
-	# compute the center of the contour area and draw a circle
-	# representing the center
-	M = cv2.moments(c)
-	cX = int(M["m10"] / M["m00"])
-	cY = int(M["m01"] / M["m00"])
-
-	# draw the countour number on the image
-	cv2.putText(image, "#{}".format(i + 1), (cX - 20, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		1.0, (255, 255, 255), 2)
-
-	# return the image with the contour number drawn on it
-	return image
-
 #------------------------------------------------------------- GET IMAGE FROM COMMAND LINE -------------------------------------------
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True,
@@ -161,34 +120,27 @@ for contour in contours:
     	image = cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
     	if ((y>150) and (y<200)):
     		so = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("So",so)
     	if ((y>200) and (y<280)):
     		ten = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("Ten",ten)
     	if ((y>300) and (y<390) and (w>100)):
     		ngaysinh = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("NgaySinh",ngaysinh)
     	if ((y>380) and (y<450) and (h>20)):
     		nguyenquan_1 = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("nguyenquan_1",nguyenquan_1)
     	if ((y>440) and (y<500)):
     		nguyenquan_2 = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("nguyenquan_2",nguyenquan_2)
     	if ((y>500) and (y<550) and (h>20)):
     		thuongtru_1 = bin_img[y:y+h,x:x+w]
+    		cv2.imshow("thuongtru_1",thuongtru_1)
     	if ((y>550) and (y<600)):
     		thuongtru_2 = bin_img[y:y+h,x:x+w]
-
-
-
-	
-      
-
+    		cv2.imshow("thuongtru_2",thuongtru_2)
 #cv2.drawContours(image, contours, -1, (0,255,0), 3)
-
 cv2.imshow("Image3",image)
-cv2.imshow("So",so)
-cv2.imshow("Ten",ten)
-cv2.imshow("NgaySinh",ngaysinh)
-cv2.imshow("nguyenquan_1",nguyenquan_1)
-cv2.imshow("nguyenquan_2",nguyenquan_2)
-cv2.imshow("thuongtru_1",thuongtru_1)
-cv2.imshow("thuongtru_2",thuongtru_2)
 cv2.waitKey(0)
 
 
